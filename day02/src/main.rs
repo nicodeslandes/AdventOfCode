5bba918ca3490166274a658356df702abd53bd02
Nicolas Deslandes <ndeslandes@live.fr>
2019-12-02 08:55:15 +0000

    Refactoring

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -14,25 +14,36 @@ fn main() -> Result<()> {
         .read_to_string(&mut input)
         .expect("Failed to read input file");
 
-    let mut values = input
+    let mut memory = input
         .split(",")
         .map(|x| x.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
-    println!("Values: {:?}", values);
+    println!("Values: {:?}", memory);
 
+    execute_program(&mut memory, 12, 2);
+    println!("Result: {}", memory[0]);
+
+    Ok(())
+}
+
+fn execute_program(memory: &mut Vec<usize>, arg1: usize, arg2: usize) -> usize {
     let mut ip: usize = 0; // Instruction pointer
 
+    // Enter parameters
+    memory[1] = arg1;
+    memory[2] = arg2;
+
     loop {
-        match read_op_code(&mut values, &mut ip) {
-            OpCode::Add => execute_instruction(&mut values, &mut ip, |a, b| a + b),
-            OpCode::Mult => execute_instruction(&mut values, &mut ip, |a, b| a * b),
+        match read_op_code(memory, &mut ip) {
+            OpCode::Add => execute_instruction(memory, &mut ip, |a, b| a + b),
+            OpCode::Mult => execute_instruction(memory, &mut ip, |a, b| a * b),
             OpCode::Exit => break,
         }
 
-        println!("Values: {:?}", values);
+        println!("Values: {:?}", memory);
     }
 
-    Ok(())
+    memory[0]
 }
 
 enum OpCode {
