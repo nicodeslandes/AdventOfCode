eb8701a15d9bb872b1f26fbde2af6ab472ca3f90
Nicolas Deslandes <ndeslandes@live.fr>
2019-12-06 07:33:04 +0000

    Day 6 - Part 1

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -9,13 +9,15 @@ type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 #[derive(Debug)]
 struct GraphNode {
     key: String,
+    parent: Option<String>,
     children: HashSet<String>,
 }
 
 impl<'a> GraphNode {
-    fn new(key: &String) -> GraphNode {
+    fn new(key: &String, parent: Option<String>) -> GraphNode {
         GraphNode {
             key: key.clone(),
+            parent: parent,
             children: HashSet::new(),
         }
     }
@@ -34,14 +36,24 @@ impl<'a> Graph {
     }
 
     fn add_node_link(&mut self, parent: &String, child: &String) {
-        self.add_or_get_node(child);
-        let parent_node = self.add_or_get_node(parent);
+        self.add_or_get_node(child, &Some(parent.clone()));
+        let parent_node = self.add_or_get_node(parent, &None);
 
         parent_node.children.insert(child.clone());
     }
 
-    fn add_or_get_node(&mut self, key: &String) -> &mut GraphNode {
-        self.nodes.entry(key.clone()).or_insert(GraphNode::new(key))
+    fn add_or_get_node(&mut self, key: &String, parent: &Option<String>) -> &mut GraphNode {
+        let node = self
+            .nodes
+            .entry(key.clone())
+            .or_insert(GraphNode::new(key, parent.clone()));
+
+        match parent {
+            Some(_p) => node.parent = parent.clone(),
+            _ => (),
+        };
+
+        node
     }
 
     fn get_node(&self, key: &String) -> Option<&GraphNode> {
@@ -72,17 +84,28 @@ fn main() -> Result<()> {
 
     println!("Orbit counts: {:?}", orbit_counts);
 
+    println!("Total count: {}", orbit_counts.values().sum::<u32>());
     Ok(())
 }
 
 fn compute_orbit_count_for(orbits: &Graph, orbit_counts: &mut HashMap<String, u32>, key: &String) {
     let node = orbits.get_node(key).unwrap();
-    if !orbit_counts.contains_key(key) {
-        for child in node.children.iter() {
-            compute_orbit_count_for(orbits, orbit_counts, &child);
-        }
-        children
+
+    // First, compute the orbit counts for the children
+    for child in node.children.iter() {
+        compute_orbit_count_for(orbits, orbit_counts, &child);
     }
+
+    // Then for the node
+    let count = match node.parent {
+        None => 0, // No orbit for the graph root
+        Some(_) => {
+            1 // direct orbit
+            + node.children.iter().map(|c| orbit_counts.get(c).unwrap()).sum::<u32>()
+        } // indirect orbits
+    };
+
+    orbit_counts.insert(node.key.clone(), count);
 }
 
 fn parse_line<'a>(line: String) -> Vec<String> {
