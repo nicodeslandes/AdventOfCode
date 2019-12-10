2eacc4529ec378f2d5000700e68f850585ba2122
Nicolas Deslandes <ndeslandes@live.fr>
2019-12-10 09:45:59 +0100

    Day 10 - part 1

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -36,6 +36,7 @@ fn main() -> Result<()> {
             let lines_of_sight =
                 compute_line_of_sight_status(&asteroids, grid_x, grid_y, Coord { x, y });
             let hit_count = count_visible_asteroids(&lines_of_sight, grid_x, grid_y);
+            println!("Visible count for {},{}: {}", x + 1, y + 1, hit_count);
             if count < hit_count {
                 count = hit_count;
             }
@@ -85,21 +86,69 @@ fn compute_line_of_sight_status(
                     result[y][x] = LineOfSightStatus::AsteroidVisible;
                     let (vector_x, vector_y) =
                         (x as i32 - asteroid.x as i32, y as i32 - asteroid.y as i32);
-                    let (mut pos_x, mut pos_y) = (x as i32 + vector_x, y as i32 + vector_y);
-                    while pos_x >= 0 && pos_x < grid_x as i32 && pos_y >= 0 && pos_y < grid_y as i32
-                    {
-                        // Hide any asteroid in that position
-                        if grid[pos_y as usize][pos_x as usize] {
-                            result[pos_y as usize][pos_x as usize] =
-                                LineOfSightStatus::LineOfSightHidden;
+                    let (dx, dy): (f64, f64) = match vector_x {
+                        0 => {
+                            // Only move the y coordinate
+                            (0.0, if vector_y > 0 { 1.0 } else { -1.0 })
                         }
-                        pos_x += vector_x;
-                        pos_y += vector_y;
+                        _ => {
+                            let dx = if vector_x > 0 { 1.0 } else { -1.0 };
+                            (dx, dx as f64 * vector_y as f64 / vector_x as f64)
+                        }
+                    };
+
+                    let mut n = 0;
+                    let mut next_pos = || {
+                        n += 1;
+                        (x as f64 + (n as f64) * dx, y as f64 + (n as f64) * dy)
+                    };
+                    let (mut pos_x, mut pos_y) = next_pos();
+                    while pos_x >= 0.0
+                        && pos_x < grid_x as f64
+                        && pos_y >= 0.0
+                        && pos_y < grid_y as f64
+                    {
+                        // if asteroid.x == 4 && asteroid.y == 7 && vector_y == 0 {
+                        //     println!("YO");
+                        // }
+
+                        // ignore any coordinates that are not on the grid
+                        if pos_x.fract() == 0.0 && pos_y.fract() == 0.0 {
+                            // Hide any asteroid in that position
+                            if grid[pos_y as usize][pos_x as usize] {
+                                result[pos_y as usize][pos_x as usize] =
+                                    LineOfSightStatus::LineOfSightHidden;
+                            }
+                        }
+
+                        let (px, py) = next_pos();
+                        pos_x = px;
+                        pos_y = py;
                     }
                 }
             }
         }
     }
+
+    // if asteroid.x == 4 && asteroid.y == 7 {
+    //     for y in 0..grid_y {
+    //         for x in 0..grid_x {
+    //             print!(
+    //                 "{}",
+    //                 if x == asteroid.x && y == asteroid.y {
+    //                     "A"
+    //                 } else {
+    //                     match result[y][x] {
+    //                         LineOfSightStatus::LineOfSightHidden => "O",
+    //                         LineOfSightStatus::AsteroidVisible => "X",
+    //                         _ => " ",
+    //                     }
+    //                 }
+    //             );
+    //         }
+    //         println!();
+    //     }
+    // }
 
     result
 }
