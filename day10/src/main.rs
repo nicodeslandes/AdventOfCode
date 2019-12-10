6253f15be757e3e72f5114e72cb0c92afbd6a1c6
Nicolas Deslandes <ndeslandes@live.fr>
2019-12-10 11:43:33 +0100

    Day 10 - Part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,9 +1,14 @@
+use ordered_float::OrderedFloat;
 use std::env;
+use std::f64::consts::PI;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+extern crate ordered_float;
 
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 type Grid<T> = Vec<Vec<T>>;
+
+#[derive(Debug)]
 struct Coord {
     x: usize,
     y: usize,
@@ -13,7 +18,7 @@ fn main() -> Result<()> {
     let file_name = env::args().nth(1).expect("Enter a file name");
     let file = File::open(file_name)?;
 
-    let asteroids: Grid<bool> = BufReader::new(file)
+    let mut asteroids: Grid<bool> = BufReader::new(file)
         .lines()
         .map(|line| {
             line.unwrap()
@@ -25,45 +30,127 @@ fn main() -> Result<()> {
 
     let grid_x = asteroids[0].len();
     let grid_y = asteroids.len();
-    println!("Asteroids grid: {}x{} {:?}", grid_x, grid_y, asteroids);
+    println!("Asteroids grid: {}x{}", grid_x, grid_y);
 
     let mut count = 0;
+    let mut found_asteroid = Coord { x: 0, y: 0 };
+
     for y in 0..grid_x {
         for x in 0..grid_y {
             if !asteroids[y][x] {
                 continue;
             }
             let lines_of_sight =
-                compute_line_of_sight_status(&asteroids, grid_x, grid_y, Coord { x, y });
-            let hit_count = count_visible_asteroids(&lines_of_sight, grid_x, grid_y);
-            println!("Visible count for {},{}: {}", x + 1, y + 1, hit_count);
+                compute_line_of_sight_status(&asteroids, grid_x, grid_y, &Coord { x, y });
+            let hit_count = get_visible_asteroids(&lines_of_sight, grid_x, grid_y).len();
+            //println!("Visible count for {},{}: {}", x + 1, y + 1, hit_count);
             if count < hit_count {
                 count = hit_count;
+                found_asteroid = Coord { x, y };
             }
         }
     }
 
-    println!("Result: {}", count);
+    let mut destroyed_asteroids = 0;
+    while destroyed_asteroids < 200 {
+        let lines_of_sight =
+            compute_line_of_sight_status(&asteroids, grid_x, grid_y, &found_asteroid);
+        let mut hit_asteroids = get_visible_asteroids(&lines_of_sight, grid_x, grid_y);
+        println!("Hit asteroids: {}", hit_asteroids.len());
+        if hit_asteroids.is_empty() {
+            break;
+        }
+
+        hit_asteroids.sort_by_key(|pos| OrderedFloat(angle_between(&found_asteroid, pos)));
+
+        println!("Blasting asteroids from {:?}", found_asteroid);
+        for hit in hit_asteroids {
+            println!(
+                "Destroying asteroid {} ({:?}, angle: {})",
+                destroyed_asteroids + 1,
+                hit,
+                angle_between(&found_asteroid, &hit) * 180.0 / PI
+            );
+            asteroids[hit.y][hit.x] = false;
+            destroyed_asteroids += 1;
+            if destroyed_asteroids == 200 {
+                println!("Found 200th asteroid: {:?}", hit);
+                break;
+            }
+        }
+    }
+
     Ok(())
 }
 
-fn count_visible_asteroids(grid: &Grid<LineOfSightStatus>, grid_x: usize, grid_y: usize) -> u32 {
-    let mut count = 0;
+fn angle_between(a: &Coord, b: &Coord) -> f64 {
+    let theta = (b.y as f64 - a.y as f64).atan2(b.x as f64 - a.x as f64);
+    let alpha = theta + PI / 2.0;
+    let alpha_mod = if alpha < 0.0 { alpha + 2.0 * PI } else { alpha };
+    //println!("theta: {}; alpha: {}; modulo, {}", theta, alpha, alpha_mod);
+    alpha_mod
+}
+
+#[test]
+fn check() {
+    let pos1 = Coord { x: 5, y: 5 };
+    let pos2 = Coord { x: 6, y: 5 };
+    println!(
+        "Angle between {:?} and {:?}: {}",
+        pos1,
+        pos2,
+        angle_between(&pos1, &pos2) * 180.0 / PI
+    )
+}
+
+#[test]
+fn check2() {
+    let pos1 = Coord { x: 5, y: 5 };
+    let pos2 = Coord { x: 5, y: 3 };
+    println!(
+        "Angle between {:?} and {:?}: {} rad , {} deg",
+        pos1,
+        pos2,
+        angle_between(&pos1, &pos2),
+        angle_between(&pos1, &pos2) * 180.0 / PI
+    )
+}
+
+#[test]
+fn check3() {
+    let pos1 = Coord { x: 5, y: 5 };
+    let pos2 = Coord { x: 3, y: 3 };
+    println!(
+        "Angle between {:?} and {:?}: {} rad , {} deg",
+        pos1,
+        pos2,
+        angle_between(&pos1, &pos2),
+        angle_between(&pos1, &pos2) * 180.0 / PI
+    )
+}
+
+fn get_visible_asteroids(
+    grid: &Grid<LineOfSightStatus>,
+    grid_x: usize,
+    grid_y: usize,
+) -> Vec<Coord> {
+    let mut result = vec![];
     for y in 0..grid_x {
         for x in 0..grid_y {
             if let LineOfSightStatus::AsteroidVisible = grid[y][x] {
-                count += 1;
+                result.push(Coord { x, y });
             }
         }
     }
 
-    count
+    result
 }
+
 fn compute_line_of_sight_status(
     grid: &Grid<bool>,
     grid_x: usize,
     grid_y: usize,
-    asteroid: Coord,
+    asteroid: &Coord,
 ) -> Grid<LineOfSightStatus> {
     let mut result = Grid::<LineOfSightStatus>::new();
     for _ in 0..grid_y {
