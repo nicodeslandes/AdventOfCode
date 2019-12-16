f3f24d4aa44817341c76cbe76473f0dda65adb2a
Nicolas Deslandes <ndeslandes@live.fr>
2019-12-16 23:02:59 +0000

    Day 16 - Part 2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -2,6 +2,8 @@ extern crate num;
 
 use std::env;
 use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 use std::io::BufReader;
 use std::io::Read;
 use std::result::Result;
@@ -12,19 +14,50 @@ fn main() -> MainResult<()> {
     let file_name = env::args().nth(1).expect("Enter a file name");
     let file = File::open(file_name)?;
 
-    let mut input = String::new();
-    BufReader::new(file).read_to_string(&mut input)?;
-    let mut input: Vec<i32> = input
+    let mut input_orig = String::new();
+    BufReader::new(file).read_to_string(&mut input_orig)?;
+    let mut input_orig: Vec<i32> = input_orig
         .chars()
         .map(|c| c.to_string().parse().unwrap())
         .collect();
 
-    println!("Input: {:?}", input);
+    println!("Input: {:?}", input_orig);
+
+    let mut input = input_orig.clone();
+    // for _ in 0..100 {
+    //     input = calculate_iteration(&input);
+    // }
+    println!("Result: {:?}", input);
+
+    input = vec![];
+    for i in 0..10_000 {
+        input.append(&mut input_orig.clone());
+    }
+
+    let offset = (0..7)
+        .map(|i| input[i])
+        .fold(0, |x: i32, i| x.abs() * 10 + i as i32) as usize;
+    println!("Index: {}; total size: {}", offset, input.len());
 
     for _ in 0..100 {
-        input = calculate_iteration(&input);
+        print!(".");
+        io::stdout().flush().unwrap();
+
+        for i in offset..input.len() - 1 {
+            let i = input.len() - 2 - i + offset;
+            input[i] = (input[i] + input[i + 1]) % 10;
+        }
     }
-    println!("Input: {:?}", input);
+
+    // for v in &input {
+    //     print!("{}", v);
+    // }
+
+    println!();
+
+    let result = (0..8).map(|i| input[i + offset]).fold(0, |x, i| x * 10 + i);
+
+    println!("Result: {}", result);
     Ok(())
 }
 
