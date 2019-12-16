dd83ab8580ce60490f5c7120124d861cdb8b64ff
Nicolas Deslandes <ndeslandes@live.fr>
2019-12-16 06:45:42 +0000

    wip

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -14,11 +14,57 @@ fn main() -> MainResult<()> {
 
     let mut input = String::new();
     BufReader::new(file).read_to_string(&mut input)?;
-    let input: Vec<i32> = input
+    let mut input: Vec<i32> = input
         .chars()
         .map(|c| c.to_string().parse().unwrap())
         .collect();
 
     println!("Input: {:?}", input);
+
+    input = calculate_iteration(&input);
+    println!("Input: {:?}", input);
     Ok(())
+}
+
+struct Pattern {
+    current_index: usize,
+    repeat_len: usize,
+    pattern_values: Vec<i32>,
+}
+
+impl Pattern {
+    fn new(repeat_len: usize) -> Pattern {
+        Pattern {
+            current_index: 0,
+            repeat_len,
+            pattern_values: vec![0, 1, 0, -1],
+        }
+    }
+}
+
+impl Iterator for Pattern {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<i32> {
+        self.current_index += 1;
+        Some(
+            self.pattern_values[(self.current_index / self.repeat_len) % self.pattern_values.len()],
+        )
+    }
+}
+
+fn calculate_iteration(input: &Vec<i32>) -> Vec<i32> {
+    let mut result = vec![];
+    for i in 0..input.len() {
+        let pattern = Pattern::new(i + 1);
+
+        let r = input
+            .iter()
+            .zip(pattern)
+            .map(|(v, p)| v * p % 10)
+            .fold(0, |x, y| (x + y) % 10);
+        result.push(r);
+    }
+
+    result
 }
