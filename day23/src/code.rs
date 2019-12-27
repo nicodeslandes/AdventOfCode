ebafd2f343d5b38813d929d3369294773db61e91
Nicolas Deslandes <ndeslandes@live.fr>
2019-12-27 16:00:51 +0100

    Refactoring

--- day23/src/code.rs
+++ day23/src/code.rs
@@ -1,14 +1,21 @@
 use crate::memory::Memory;
+use std::collections::VecDeque;
 
 pub struct Computer {
     context: ExecutionContext,
+    input: VecDeque<i64>,
 }
 
 impl Computer {
     pub fn new(memory: &Memory) -> Computer {
         Computer {
             context: ExecutionContext::new(memory),
+            input: VecDeque::new(),
         }
+    }
+
+    fn read_input(&mut self) -> Option<i64> {
+        self.input.pop_back()
     }
 
     pub fn execute(&mut self) -> ExecutionResult {
@@ -22,77 +29,83 @@ impl Computer {
     }
 
     pub fn execute_single_instruction(&mut self) -> ExecutionResult {
-        let context = &mut self.context;
-        match read_op_code(context) {
+        match self.context.read_op_code() {
             (OpCode::Add, parameter_modes) => {
-                let (a, b, c) = extract_parameters3(context, parameter_modes);
-                c.set(a.get(context) + b.get(context), context);
+                let (a, b, c) = self.context.extract_parameters3(parameter_modes);
+                c.set(
+                    a.get(&mut self.context) + b.get(&mut self.context),
+                    &mut self.context,
+                );
             }
             (OpCode::Mult, parameter_modes) => {
-                let (a, b, c) = extract_parameters3(context, parameter_modes);
-                c.set(a.get(context) * b.get(context), context);
+                let (a, b, c) = self.context.extract_parameters3(parameter_modes);
+                c.set(
+                    a.get(&self.context) * b.get(&self.context),
+                    &mut self.context,
+                );
             }
             (OpCode::Input, parameter_modes) => {
-                match context.read_input() {
+                match self.read_input() {
                     Some(value) => {
                         // println!("Reading input {}", value);
-                        let a = extract_parameter(context, parameter_modes);
-                        a.set(value, context);
+                        let a = self.context.extract_parameter(parameter_modes);
+                        a.set(value, &mut self.context);
                     }
                     None => {
                         //println!("Halting program due to input read; ip: {}", context.ip);
                         // Revert the reading of the op-code, so we can read it again when the
                         // thread is resumed
-                        context.ip -= 1;
+                        self.context.ip -= 1;
                         return ExecutionResult::MoreInputNeeded;
                     }
                 }
             }
             (OpCode::Output, parameter_modes) => {
-                let a = extract_parameter(context, parameter_modes);
-                let output = a.get(&context);
+                let a = self.context.extract_parameter(parameter_modes);
+                let output = a.get(&self.context);
                 //println!("Output: {}", output);
-                context.write_output(output);
+                self.context.write_output(output);
             }
             (OpCode::JumpIfTrue, parameter_modes) => {
-                let (a, b) = extract_parameters2(context, parameter_modes);
-                if a.get(&context) != 0 {
-                    let address = b.get(&context);
-                    jump_to(&mut context.ip, address);
+                let (a, b) = self.context.extract_parameters2(parameter_modes);
+                if a.get(&self.context) != 0 {
+                    let address = b.get(&self.context);
+                    self.context.jump_to(address);
                 }
             }
             (OpCode::JumpIfFalse, parameter_modes) => {
-                let (a, b) = extract_parameters2(context, parameter_modes);
-                if a.get(&context) == 0 {
-                    let address = b.get(&context);
-                    jump_to(&mut context.ip, address);
+                let (a, b) = self.context.extract_parameters2(parameter_modes);
+                if a.get(&self.context) == 0 {
+                    let address = b.get(&self.context);
+                    self.context.jump_to(address);
                 }
             }
             (OpCode::LessThan, parameter_modes) => {
-                let (a, b, c) = extract_parameters3(context, parameter_modes);
-                let value = if a.get(&context) < b.get(&context) {
+                let (a, b, c) = self.context.extract_parameters3(parameter_modes);
+                let value = if a.get(&self.context) < b.get(&self.context) {
                     1
                 } else {
                     0
                 };
-                c.set(value, context);
+                c.set(value, &mut self.context);
             }
             (OpCode::Equals, parameter_modes) => {
-                let (a, b, c) = extract_parameters3(context, parameter_modes);
-                let value = if a.get(&context) == b.get(&context) {
+                let (a, b, c) = self.context.extract_parameters3(parameter_modes);
+                let value = if a.get(&self.context) == b.get(&self.context) {
                     1
                 } else {
                     0
                 };
-                c.set(value, context);
+                c.set(value, &mut self.context);
             }
             (OpCode::AdjustRelativeBase, parameter_modes) => {
-                let a = extract_parameter(context, parameter_modes);
-                let adjustment = a.get(&context);
-                context.relative_base = (context.relative_base as i64 + adjustment) as usize;
+                let a = self.context.extract_parameter(parameter_modes);
+                let adjustment = a.get(&self.context);
+                self.context.relative_base =
+                    (self.context.relative_base as i64 + adjustment) as usize;
             }
             (OpCode::Exit, _) => {
-                context.ended = true;
+                self.context.ended = true;
                 return ExecutionResult::Exit;
             }
         };
@@ -138,19 +151,6 @@ impl ExecutionContext {
         }
     }
 
-    fn read_input(&mut self) -> Option<i64> {
-        if self.input_index >= self.input.len() {
-            self.input_index = 0;
-            self.input.clear();
-            None
-        } else {
-            let res = self.input[self.input_index];
-            print_char(res);
-            self.input_index += 1;
-            Some(res)
-        }
-    }
-
     fn write_output(&mut self, value: i64) {
         if value > 255 {
             println!("Result: {}", value);
@@ -159,6 +159,75 @@ impl ExecutionContext {
         }
         self.output = value;
         //self.output.clear();
+    }
+
+    fn jump_to(&mut self, address: i64) {
+        self.ip = address as usize;
+    }
+
+    fn read_op_code(&mut self) -> (OpCode, u32) {
+        let value = self.memory[self.ip];
+        let op_code_value = value % 100;
+        let parameter_modes = (value / 100) as u32;
+
+        let op_code = match op_code_value {
+            1 => OpCode::Add,
+            2 => OpCode::Mult,
+            3 => OpCode::Input,
+            4 => OpCode::Output,
+            5 => OpCode::JumpIfTrue,
+            6 => OpCode::JumpIfFalse,
+            7 => OpCode::LessThan,
+            8 => OpCode::Equals,
+            9 => OpCode::AdjustRelativeBase,
+            99 => OpCode::Exit,
+            x => panic!("Unknown op code: {}; ip: {}", x, self.ip),
+        };
+
+        self.ip += 1;
+        (op_code, parameter_modes)
+    }
+    fn extract_parameter(&mut self, parameter_modes: u32) -> Parameter {
+        let mut param_modes = parameter_modes;
+        self.get_parameter(&mut param_modes)
+    }
+
+    fn extract_parameters2(&mut self, parameter_modes: u32) -> (Parameter, Parameter) {
+        let mut param_modes = parameter_modes;
+        let x = self.get_parameter(&mut param_modes);
+        let y = self.get_parameter(&mut param_modes);
+        (x, y)
+    }
+
+    fn extract_parameters3(&mut self, parameter_modes: u32) -> (Parameter, Parameter, Parameter) {
+        let mut param_modes = parameter_modes;
+        let x = self.get_parameter(&mut param_modes);
+        let y = self.get_parameter(&mut param_modes);
+        let z = self.get_parameter(&mut param_modes);
+        (x, y, z)
+    }
+
+    fn get_parameter(&mut self, parameter_modes: &mut u32) -> Parameter {
+        // Get the parameter mode for this parameter
+        let parameter_mode = match *parameter_modes % 10 {
+            0 => ParameterMode::Position,
+            1 => ParameterMode::Immediate,
+            2 => ParameterMode::Relative,
+            x => panic!(format!("Incorrect parameter mode: {}", x)),
+        };
+        *parameter_modes /= 10;
+
+        let parameter_value = self.memory[self.ip];
+        self.ip += 1;
+
+        match parameter_mode {
+            ParameterMode::Position => Parameter::Reference(parameter_value as usize),
+            ParameterMode::Immediate => Parameter::ImmediateValue(parameter_value),
+            ParameterMode::Relative => {
+                let address = (parameter_value + self.relative_base as i64) as usize;
+                Parameter::Reference(address)
+            }
+        }
     }
 }
 
@@ -175,82 +244,6 @@ pub enum ExecutionResult {
     Executed,
     MoreInputNeeded,
     Exit,
-}
-
-fn jump_to(ip: &mut usize, address: i64) {
-    *ip = address as usize;
-}
-
-fn read_op_code(context: &mut ExecutionContext) -> (OpCode, u32) {
-    let value = context.memory[context.ip];
-    let op_code_value = value % 100;
-    let parameter_modes = (value / 100) as u32;
-
-    let op_code = match op_code_value {
-        1 => OpCode::Add,
-        2 => OpCode::Mult,
-        3 => OpCode::Input,
-        4 => OpCode::Output,
-        5 => OpCode::JumpIfTrue,
-        6 => OpCode::JumpIfFalse,
-        7 => OpCode::LessThan,
-        8 => OpCode::Equals,
-        9 => OpCode::AdjustRelativeBase,
-        99 => OpCode::Exit,
-        x => panic!("Unknown op code: {}; ip: {}", x, context.ip),
-    };
-
-    context.ip += 1;
-    (op_code, parameter_modes)
-}
-
-fn extract_parameter(context: &mut ExecutionContext, parameter_modes: u32) -> Parameter {
-    let mut param_modes = parameter_modes;
-    get_parameter(context, &mut param_modes)
-}
-
-fn extract_parameters2(
-    context: &mut ExecutionContext,
-    parameter_modes: u32,
-) -> (Parameter, Parameter) {
-    let mut param_modes = parameter_modes;
-    let x = get_parameter(context, &mut param_modes);
-    let y = get_parameter(context, &mut param_modes);
-    (x, y)
-}
-
-fn extract_parameters3(
-    context: &mut ExecutionContext,
-    parameter_modes: u32,
-) -> (Parameter, Parameter, Parameter) {
-    let mut param_modes = parameter_modes;
-    let x = get_parameter(context, &mut param_modes);
-    let y = get_parameter(context, &mut param_modes);
-    let z = get_parameter(context, &mut param_modes);
-    (x, y, z)
-}
-
-fn get_parameter(context: &mut ExecutionContext, parameter_modes: &mut u32) -> Parameter {
-    // Get the parameter mode for this parameter
-    let parameter_mode = match *parameter_modes % 10 {
-        0 => ParameterMode::Position,
-        1 => ParameterMode::Immediate,
-        2 => ParameterMode::Relative,
-        x => panic!(format!("Incorrect parameter mode: {}", x)),
-    };
-    *parameter_modes /= 10;
-
-    let parameter_value = context.memory[context.ip];
-    context.ip += 1;
-
-    match parameter_mode {
-        ParameterMode::Position => Parameter::Reference(parameter_value as usize),
-        ParameterMode::Immediate => Parameter::ImmediateValue(parameter_value),
-        ParameterMode::Relative => {
-            let address = (parameter_value + context.relative_base as i64) as usize;
-            Parameter::Reference(address)
-        }
-    }
 }
 
 enum Parameter {
