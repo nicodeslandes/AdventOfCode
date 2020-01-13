c44359a9d3b687118fb84fcb62b466a6c6fad1d4
Nicolas Deslandes <ndeslandes@live.fr>
2020-01-13 11:42:35 +0000

    wip

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,6 +1,9 @@
 use crate::code::{Computer, ExecutionResult::*};
 use crate::memory::Memory;
+use std::cell::Cell;
+use std::collections::VecDeque;
 use std::env;
+use std::rc::Rc;
 
 #[cfg(unix)]
 extern crate ncurses;
@@ -17,9 +20,20 @@ fn main() -> Result<()> {
     let file_name = env::args().nth(1).expect("Enter a file name");
 
     let memory = Memory::load_from_file(&file_name)?;
-    let input = || None;
+
+    let values: VecDeque<i64> = VecDeque::new();
+    let values = Rc::new(Cell::new(values));
+    //let v = Rc::get_mut(values.get_mut()).unwrap();
+
+    let input = || {
+        let v = values.get_mut();
+        v.pop_back()
+    };
+
     let output = |x| {
         println!("Output: {}", x);
+        let v = values.get_mut();
+        v.push_front(x);
     };
     let mut computer = Computer::new(&memory, Box::new(input), Box::new(output));
 
