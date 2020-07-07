046d966434a84a68f4ad11374a409a3917f69925
Nicolas Deslandes <ndeslandes@live.fr>
2020-07-07 19:28:39 +0100

    Day24 - Part 1

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,32 +1,94 @@
-use std::collections::HashMap;
+use array2d::Array2D;
+use std::collections::HashSet;
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::iter::FromIterator;
 use std::result::Result;
 
 type MainResult<T> = Result<T, Box<dyn ::std::error::Error>>;
-
-#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
-struct Pos(usize, usize);
-
-#[derive(Debug)]
-enum Content {
-    Wall,
-    Passage,
-    Portal(String),
-}
 
 fn main() -> MainResult<()> {
     let file_name = env::args().nth(1).expect("Enter a file name");
     let file = File::open(file_name)?;
 
-    let mut character_grid: HashMap<Pos, char> = HashMap::new();
-    let mut grid: HashMap<Pos, Content> = HashMap::new();
+    let mut grid = read_grid_from_file(file)?;
 
+    let mut grid_biodiversities: HashSet<i32> = HashSet::new();
+    loop {
+        println!("Grid:");
+        display_grid(&grid);
+        let b = calculate_biodiversity(&grid);
+        println!("Biodiversity = {}", b);
+        if !grid_biodiversities.insert(b) {
+            println!("Found duplicate! Result: {}", b);
+            break;
+        }
+
+        evolve(&mut grid);
+    }
+
+    Ok(())
+}
+
+type Grid = Array2D<bool>;
+
+fn evolve(grid: &mut Grid) -> () {
+    let original = grid.clone();
+
+    let nb_adjacent_bugs = |x: usize, y: usize| {
+        let bug_left = x > 0 && *original.get(x - 1, y).unwrap();
+        let bug_right = *original.get(x + 1, y).unwrap_or(&false);
+        let bug_top = y > 0 && *original.get(x, y - 1).unwrap();
+        let bug_bottom = *original.get(x, y + 1).unwrap_or(&false);
+        vec![bug_left, bug_right, bug_top, bug_bottom]
+            .into_iter()
+            .filter(|x| *x)
+            .count()
+    };
+
+    for x in 0..grid.column_len() {
+        for y in 0..grid.row_len() {
+            let adjacent_bug_count = nb_adjacent_bugs(x, y);
+            let mut bug = *original.get(x, y).unwrap();
+            if bug {
+                if adjacent_bug_count != 1 {
+                    bug = false
+                }
+            } else {
+                if adjacent_bug_count == 1 || adjacent_bug_count == 2 {
+                    bug = true
+                }
+            }
+
+            grid.set(x, y, bug).unwrap();
+        }
+    }
+}
+
+fn display_grid(grid: &Grid) -> () {
+    for row in grid.rows_iter() {
+        for &el in row {
+            print!("{}", if el { "#" } else { "." });
+        }
+        println!();
+    }
+}
+
+fn calculate_biodiversity(grid: &Grid) -> i32 {
+    let mut result = 0;
+    for (i, &el) in grid.elements_row_major_iter().enumerate() {
+        if el {
+            result += 1 << i;
+        }
+    }
+
+    result
+}
+fn read_grid_from_file(file: File) -> Result<Grid, Box<dyn Error>> {
     let mut reader = BufReader::new(&file);
-    let mut y = 0;
+    let mut parsed_grid: Vec<bool> = vec![];
     loop {
         let mut line = String::new();
         let read = reader.read_line(&mut line)?;
@@ -34,160 +96,12 @@ fn main() -> MainResult<()> {
             break;
         }
 
-        for (x, ch) in line.chars().enumerate() {
-            let pos = Pos(x, y);
-            character_grid.insert(pos, ch);
-        }
-
-        y += 1;
-    }
-
-    let x_max = character_grid.keys().map(|Pos(x, _)| *x).max().unwrap();
-    let y_max = character_grid.keys().map(|Pos(_, y)| *y).max().unwrap();
-
-    let read_portal_name = |pos1: Pos, pos2: Pos| {
-        let chars = vec![character_grid[&pos1], character_grid[&pos2]];
-        String::from_iter(chars.into_iter())
-    };
-
-    let mut portals: Vec<(String, Pos)> = vec![];
-
-    let mut gen_portal = |pos: Pos, pos1: Pos, pos2: Pos| {
-        let name = read_portal_name(pos1, pos2);
-        portals.push((name.clone(), pos));
-        Content::Portal(name)
-    };
-
-    for y in 2..y_max - 1 {
-        for x in 2..x_max - 1 {
-            let pos = Pos(x, y);
-            let content = match character_grid.get(&pos) {
-                Some('.') => {
-                    if x == 2 {
-                        gen_portal(pos, Pos(x - 2, y), Pos(x - 1, y))
-                    } else if x == x_max - 1 {
-                        gen_portal(pos, Pos(x + 1, y), Pos(x + 2, y))
-                    } else if y == 2 {
-                        gen_portal(pos, Pos(x, y - 2), Pos(x, y - 1))
-                    } else if y == y_max - 1 {
-                        gen_portal(pos, Pos(x, y + 1), Pos(x, y + 2))
-                    } else {
-                        Content::Passage
-                    }
-                }
-                Some('#') => Content::Wall,
-                _ => continue,
-            };
-
-            grid.insert(pos, content);
-        }
-    }
-
-    println!("Grid: {:?}", grid);
-
-    let current = *grid
-        .iter()
-        .find(|(_, v)| match v {
-            Content::Portal(s) => s == "AA",
-            _ => false,
-        })
-        .unwrap()
-        .0;
-
-    println!("Start position: {:?}", current);
-
-    let mut state: Vec<Vec<State>> = vec![];
-    for x in 0..x_max {
-        state.push(vec![State::None; y_max + 1]);
-        for y in 0..y_max {
-            state[x][y] = match grid.get(&Pos(x, y)) {
-                Some(Content::Wall) => State::Wall,
-                _ => State::None,
+        for ch in line.chars() {
+            if ch != '\n' && ch != '\r' {
+                parsed_grid.push(ch == '#');
             }
         }
     }
 
-    for y in 0..y_max {
-        for x in 0..x_max {
-            if current == Pos(x, y) {
-                print!("x");
-            } else {
-                match state[x][y] {
-                    State::Wall => print!("#"),
-                    State::Visited(n) => print!("{}", n % 10),
-                    _ => print!(" "),
-                }
-            }
-        }
-        println!();
-    }
-
-    // loop {
-    //     match grid.get(&current){
-    //         Some(Content::Passage) {
-    //             for
-    //         }
-    //     }
-    // }
-
-    Ok(())
-}
-
-#[derive(Copy, Clone, Debug)]
-enum State {
-    Wall,
-    None,
-    Visited(u32),
-}
-
-struct NextMoveIterator {
-    next_direction: Option<Direction>,
-    origin: Pos,
-}
-
-impl NextMoveIterator {
-    fn new(pos: Pos) -> NextMoveIterator {
-        NextMoveIterator {
-            origin: pos,
-            next_direction: Some(Direction::Up),
-        }
-    }
-}
-
-impl Iterator for NextMoveIterator {
-    type Item = Pos;
-
-    fn next(&mut self) -> Option<Pos> {
-        match &self.next_direction {
-            None => None,
-            Some(d) => {
-                let Pos(x, y) = self.origin;
-                match d {
-                    Direction::Up => {
-                        self.next_direction = Some(Direction::Right);
-                        Some(Pos(x, y - 1))
-                    }
-                    Direction::Right => {
-                        self.next_direction = Some(Direction::Bottom);
-                        Some(Pos(x, y - 1))
-                    }
-                    Direction::Bottom => {
-                        self.next_direction = Some(Direction::Left);
-                        Some(Pos(x, y - 1))
-                    }
-                    Direction::Left => {
-                        self.next_direction = None;
-                        Some(Pos(x, y - 1))
-                    }
-                }
-            }
-        }
-    }
-}
-
-enum Direction {
-    Up,
-    Right,
-    Bottom,
-    Left,
+    return Ok(Grid::from_row_major(&parsed_grid, 5, 5));
 }
