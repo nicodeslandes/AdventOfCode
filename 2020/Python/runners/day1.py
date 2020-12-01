6b965f4389b557d81a0b37adc4161d04d99e4c48
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-01 08:20:38 +0000

    Day 1 - Part 2

--- 2020/Python/runners/day1.py
+++ 2020/Python/runners/day1.py
@@ -8,4 +8,11 @@ def part1(input: List[str]) -> int:
                 return values[i] * values[j]
     return 0
 
-# def part2(input: List[str]) -> int:
+def part2(input: List[str]) -> int:
+    values = [int(x) for x in input]
+    for i in range(len(values)):
+        for j in range(i, len(values)):
+            for k in range(j, len(values)):
+                if values[i] + values[j] + values[k] == 2020:
+                    return values[i] * values[j] * values[k]
+    return 0
