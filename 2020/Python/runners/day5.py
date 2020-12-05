902449b4de178955b54588d795bc80e638e047f5
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-05 22:33:18 +0000

    day 5 part 2

--- 2020/Python/runners/day5.py
+++ 2020/Python/runners/day5.py
@@ -1,4 +1,5 @@
 from typing import List
+from logging import debug, info
 
 
 def read_seat(line):
@@ -10,11 +11,32 @@ def read_seat(line):
     for i in range(7, 10):
         col = col * 2 + int(line[i] == 'R')
 
-    return row * 8 + col
+    return row, col
 
 
 def part1(input: List[str]) -> int:
-    return max(read_seat(line) for line in input)
+    return max(row * 8 + col for line in input for row, col in [read_seat(line)])
 
 
 def part2(input: List[str]) -> int:
+    seats = [read_seat(line) for line in input]
+    seats.sort()
+
+    def find_missing_seat():
+        for i in range(1, len(seats)):
+            (r1, c1) = seats[i]
+            (r2, c2) = seats[i+1]
+            if r1 == r2:
+                if c2 != c1 + 1:
+                    return r1, c1 + 1
+            else:
+                if c1 != 7:
+                    return r1, 7
+                if c2 != 0:
+                    return r2, 0
+
+        raise Exception("Couldn't find missing seat")
+
+    r, c = find_missing_seat()
+    info("Found seat: %s", (r, c))
+    return r * 8 + c
