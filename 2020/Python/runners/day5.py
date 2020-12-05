0c6d415384411d4184b219d793ece558fc3a3ab1
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-05 22:37:29 +0000

    Simplified solution for day 5

--- 2020/Python/runners/day5.py
+++ 2020/Python/runners/day5.py
@@ -1,4 +1,5 @@
-from typing import List
+from runners.utils import findfirst, zipwithnext
+from typing import List, Tuple
 from logging import debug, info
 
 
@@ -14,29 +15,16 @@ def read_seat(line):
     return row, col
 
 
+def to_seat_id(seat: Tuple[int, int]):
+    row, col = seat
+    return row * 8 + col
+
+
 def part1(input: List[str]) -> int:
     return max(row * 8 + col for line in input for row, col in [read_seat(line)])
 
 
 def part2(input: List[str]) -> int:
-    seats = [read_seat(line) for line in input]
+    seats = [to_seat_id(read_seat(line)) for line in input]
     seats.sort()
-
-    def find_missing_seat():
-        for i in range(1, len(seats)):
-            (r1, c1) = seats[i]
-            (r2, c2) = seats[i+1]
-            if r1 == r2:
-                if c2 != c1 + 1:
-                    return r1, c1 + 1
-            else:
-                if c1 != 7:
-                    return r1, 7
-                if c2 != 0:
-                    return r2, 0
-
-        raise Exception("Couldn't find missing seat")
-
-    r, c = find_missing_seat()
-    info("Found seat: %s", (r, c))
-    return r * 8 + c
+    return findfirst(zipwithnext(seats), lambda t: t[1] != t[0] + 1)[0] + 1
--- 2020/Python/runners/utils.py
+++ 2020/Python/runners/utils.py
@@ -1,11 +1,12 @@
 import logging
-from typing import Iterable, Iterator, TypeVar
+from typing import Callable, Iterable, Iterator, Optional, Tuple, TypeVar
 from functools import reduce
 
 T = TypeVar("T")
+TNumeric = TypeVar("TNumeric", int, float)
 
 
-def product(l: Iterable[T]) -> T:
+def product(l: Iterable[TNumeric]) -> TNumeric:
     return reduce(lambda acc, x: acc * x, l, 1)
 
 
@@ -15,3 +16,22 @@ def count(items: Iterator[T]) -> int:
 
 def isEnabled(level: int) -> bool:
     return logging.getLogger().isEnabledFor(level)
+
+
+def zipwithnext(src: Iterable[T]) -> Iterable[Tuple[T, T]]:
+    iter = src.__iter__()
+    try:
+        prev = iter.__next__()
+        while True:
+            current = iter.__next__()
+            yield prev, current
+            prev = current
+    except StopIteration:
+        pass
+
+
+def findfirst(src: Iterable[T], pred: Callable[[T], bool]) -> Optional[T]:
+    for item in src:
+        if pred(item):
+            return item
+    return None
