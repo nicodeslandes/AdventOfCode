586c1370866d9466686f70893367765bfef05801
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-06 05:19:37 +0000

    Day 6

---
+++ 2020/Python/runners/day6.py
@@ -0,0 +1,51 @@
+from typing import List
+
+
+def part1(input: List[str]) -> int:
+    count = 0
+
+    def process_group(group: List[str]):
+        answers = {}
+        for pax in group:
+            for answer in pax:
+                answers[answer] = 1
+
+        nonlocal count
+        count += len(answers)
+
+    group = []
+    for line in input:
+        if len(line) == 0:
+            process_group(group)
+            group.clear()
+        group.append(line)
+
+    process_group(group)
+    return count
+
+
+def part2(input: List[str]) -> int:
+    total = 0
+
+    def process_group(group: List[str]):
+        answers = {}
+        for pax in group:
+            for answer in pax:
+                c = answers.get(answer)
+                answers[answer] = c + 1 if c else 1
+
+        gl = len(group)
+        nonlocal total
+        s = sum((1 for answer in answers if answers[answer] == gl))
+        total += s
+
+    group = []
+    for line in input:
+        if len(line) == 0:
+            process_group(group)
+            group.clear()
+        else:
+            group.append(line)
+
+    process_group(group)
+    return total
