69c708f56569a5af3e64befb368185e09a5fc0a7
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-09 07:00:51 +0000

    Day 9 - part 2

--- 2020/Python/runners/day9.py
+++ 2020/Python/runners/day9.py
@@ -26,6 +26,39 @@ def part1(input: List[str], is_test: bool) -> int:
         last_n.append(n)
         last_n.pop(0)
 
-    return -1
+    raise Exception("Could not find weakness")
 
-# def part2(input: List[str]) -> int:
+
+def part2(input: List[str], is_test: bool) -> int:
+    preamble_length = 5 if is_test else 25
+    numbers = list(int(x) for x in input)
+    last_n = []
+    weakness = None
+    for n in numbers:
+        if len(last_n) < preamble_length:
+            last_n.append(n)
+            continue
+
+        if not is_pair_sum(n, last_n):
+            weakness = n
+        last_n.append(n)
+        last_n.pop(0)
+
+    if not weakness:
+        raise Exception("Could not find weakness")
+
+    prefixes = []
+    psum = 0
+    for n in numbers:
+        psum += n
+        prefixes.append(psum)
+
+    for i in range(len(prefixes)):
+        for j in range(i):
+            if prefixes[i] - prefixes[j] == weakness:
+                debug("Found chain! From %d to %d", numbers[i], numbers[j+1])
+                mi = min([numbers[k] for k in range(j+1, i)])
+                ma = max([numbers[k] for k in range(j+1, i)])
+                return mi + ma
+
+    raise Exception("Failed")
