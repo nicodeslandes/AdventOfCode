5a068337ac58eb6dc4b8581c8cbe6eab75b0ac70
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-10 06:14:02 +0000

    Day 10 - Part 2

--- 2020/Python/runners/day10.py
+++ 2020/Python/runners/day10.py
@@ -23,6 +23,9 @@ def part2(input: List[str]) -> int:
     jolts = sorted([int(x) for x in input])
     diffs = {}
     jolts.insert(0, 0)
+    jolts.append(max(jolts) + 3)
+
+    debug("Jolts: %s", jolts)
 
     for i in range(len(jolts) - 1):
         d = jolts[i+1] - jolts[i]
@@ -32,16 +35,16 @@ def part2(input: List[str]) -> int:
         else:
             diffs[d] += 1
 
-    diffs[3] += 1
+    #diffs[3] += 1
     info("Diffs: %s", diffs)
 
     def get_jolt_diff(index: int):
         current = jolts[index]
-        if index >= len(jolts) - 1:
-            return 3
-        else:
-            next = jolts[index + 1]
-            return next - current
+        # if index >= len(jolts) - 1:
+        #     return 3
+        # else:
+        next = jolts[index + 1]
+        return next - current
 
     memo: Dict[int, int] = {}
 
@@ -50,9 +53,9 @@ def part2(input: List[str]) -> int:
         if cached is not None:
             return cached
 
-        if start_index == len(jolts):
+        if start_index == len(jolts) - 1:
             return 1
-        if start_index > len(jolts):
+        if start_index >= len(jolts):
             return 0
 
         diff = get_jolt_diff(start_index)
@@ -63,12 +66,12 @@ def part2(input: List[str]) -> int:
             else:
                 next_diff = get_jolt_diff(start_index + 1)
                 if next_diff == 1:
-                    # We have 1-1 diffs, can skip current
                     next_next_diff = get_jolt_diff(start_index + 2)
                     if next_next_diff == 1:
                         # We have 1-1-1, we can skip current, and next
                         return combination_count(start_index + 1) + combination_count(start_index+2) + combination_count(start_index+3)
                     else:
+                        # We have 1-1-3 diffs, can skip current
                         return combination_count(start_index + 1) + combination_count(start_index+2)
                 else:
                     # We have 1-3
