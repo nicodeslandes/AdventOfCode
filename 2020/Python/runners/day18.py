1a4ae8af42cb23db802dea8cd5df6beb4db72942
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-18 08:35:49 +0000

    Day 18 - Part 2

--- 2020/Python/runners/day18.py
+++ 2020/Python/runners/day18.py
@@ -3,7 +3,7 @@ import re
 from logging import debug, info
 
 
-def execute(expr: str, level=0) -> int:
+def execute1(expr: str, level=0) -> int:
     if level == 0:
         info("Evaluating %s", expr)
     else:
@@ -11,19 +11,39 @@ def execute(expr: str, level=0) -> int:
 
     if m := re.search(r"\(([^\(\)]+)\)", expr):
         sub = m.group(1)
-        return execute(expr.replace(f"({sub})", f"{execute(sub, level + 1)}", 1), level + 1)
+        return execute1(expr.replace(f"({sub})", f"{execute1(sub, level + 1)}", 1), level + 1)
 
     if m := re.search(r"(\d+) ([+*]) (\d+)", expr):
         a = int(m.group(1))
         op = m.group(2)
         b = int(m.group(3))
         r = a + b if op == '+' else a * b
-        return execute(expr.replace(f"{a} {op} {b}", f"{r}", 1), level + 1)
+        return execute1(expr.replace(f"{a} {op} {b}", f"{r}", 1), level + 1)
 
-    # if m := re.search(r"(\d+) \* (\d+)", expr):
-    #     a = int(m.group(1))
-    #     b = int(m.group(2))
-    #     return execute(expr.replace(f"{a} * {b}", f"{a*b}"))
+    if level == 0:
+        info("Result: %s", expr)
+    return int(expr)
+
+
+def execute2(expr: str, level=0) -> int:
+    if level == 0:
+        info("Evaluating %s", expr)
+    else:
+        debug("%sEvaluating %s", " "*level, expr)
+
+    if m := re.search(r"\(([^\(\)]+)\)", expr):
+        sub = m.group(1)
+        return execute2(expr.replace(f"({sub})", f"{execute2(sub, level + 1)}", 1), level + 1)
+
+    if m := re.search(r"(\d+) \+ (\d+)", expr):
+        a = int(m.group(1))
+        b = int(m.group(2))
+        return execute2(expr.replace(f"{a} + {b}", f"{a+b}", 1), level + 1)
+
+    if m := re.search(r"(\d+) \* (\d+)", expr):
+        a = int(m.group(1))
+        b = int(m.group(2))
+        return execute2(expr.replace(f"{a} * {b}", f"{a*b}", 1), level + 1)
 
     if level == 0:
         info("Result: %s", expr)
@@ -31,6 +51,8 @@ def execute(expr: str, level=0) -> int:
 
 
 def part1(input: List[str]) -> int:
-    return sum(execute(e) for e in input)
+    return sum(execute1(e) for e in input)
 
-# def part2(input: List[str]) -> int:
+
+def part2(input: List[str]) -> int:
+    return sum(execute2(e) for e in input)
