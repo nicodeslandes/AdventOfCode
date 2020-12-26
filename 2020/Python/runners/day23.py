93bafd09f0dd96a77e187b9b126afc325730bed5
Nicolas Deslandes <ndeslandes@live.fr>
2020-12-26 19:37:50 +0000

    Day 23 - Part 2

--- 2020/Python/runners/day23.py
+++ 2020/Python/runners/day23.py
@@ -1,9 +1,7 @@
 import logging
 from runners.utils import isEnabled
 from typing import List, Optional, Tuple
-from logging import debug
-
-N = 10
+from logging import debug, info
 
 Cups = List[int]
 
@@ -12,12 +10,12 @@ def print_cups(cup: int, cups: Cups, n=None) -> str:
     c = cup
     labels = []
     for i in range(5 if not n else n):
-        labels.append(c)
+        labels.append(c + 1)
         c = cups[c]
     return str(labels)
 
 
-def next(cup: int, cups: Cups) -> Tuple[int, Cups]:
+def next(cup: int, cups: Cups) -> int:
     removed = []
     ri = cups[cup]
     for i in range(3):
@@ -27,8 +25,8 @@ def next(cup: int, cups: Cups) -> Tuple[int, Cups]:
     destination = cup
     while True:
         destination -= 1
-        if destination <= 0:
-            destination = N
+        if destination < 0:
+            destination = len(cups) - 1
 
         if not destination in removed:
             break
@@ -43,90 +41,58 @@ def next(cup: int, cups: Cups) -> Tuple[int, Cups]:
 
     # debug("After dest cup %d: %s", destination, print_cups(dest_cup))
 
-    return cups[cup]
+    return ri
 
 
-def play_rounds(cups: Cup, iterations: int) -> Cup:
-    # memo = set()
+def play_rounds(cup: int, cups: Cups, iterations: int) -> Tuple[int, Cups]:
     for i in range(iterations):
-        #debug("Cups: %s", cups)
-        if i % 100_000 == 0:
-            print(".", end='')
-        # state = tuple(cups)
-        # if state in memo:
-        #     raise Exception(f"Yo! i = {i}")
-        # else:
-        # memo.add(state)
+        # if i % 100_000 == 0:
+        #     print(".", end='')
         if isEnabled(logging.DEBUG):
-            debug("Cups: %s", print_cups(cups, 10))
-        cups = next(cups)
+            debug("Cups %d: %s", i+1, print_cups(cup, cups, 10))
+        cup = next(cup, cups)
+    return cup, cups
+
+
+def parse_cups(input: str) -> Cups:
+    cups = [0 for i in input]
+    p = None
+    for c in input:
+        i = int(c) - 1
+        if p is not None:
+            cups[p] = i
+        p = i
+
+    cups[p] = int(input[0]) - 1
     return cups
 
 
-def parse_cups(input: str) -> Cup:
-    cup: Optional[Cup] = None
-    returned_cup: Optional[Cup] = None
-    for c in input:
-        new = Cup(int(c))
-        if cup is not None:
-            cup.next = new
-        else:
-            returned_cup = new
-        cup = new
-
-    global N
-    N = len(input)
-    # Circle back to the 1st one
-    if returned_cup is None:
-        raise Exception("What !?")
-    cup.next = returned_cup
-    return returned_cup
-
-
-def throw(error: str) -> Cup:
-    raise Exception(error)
-
-
-def find_cup(from_cup: Cup, label: int) -> Cup:
-    if from_cup.label == label:
-        return from_cup
-    c: Cup = from_cup.next or throw("Null")
-    if c.label == label:
-        return c
-    while c != from_cup:
-        if c.label == label:
-            return c
-        else:
-            c = c.next or throw("Null")
-    raise Exception(f"Could not find label %d", label)
-
-
 def part1(input: List[str]) -> str:
-    cup = parse_cups(input[0])
-    cup = play_rounds(cup, 100)
+    cups = parse_cups(input[0])
+    debug("Cups: %s", cups)
+    start = int(input[0][0]) - 1
+    _, cups = play_rounds(start, cups, 100)
 
-    one_cup = find_cup(cup, 1)
+    c = 0
     labels = []
-    c = one_cup.next
-    while c != one_cup:
-        labels.append(str(c.label))
-        c = c.next
+    for i in range(1, len(cups)):
+        labels.append(str(cups[c] + 1))
+        c = cups[c]
+
     return "".join(labels)
 
 
 def part2(input: List[str]) -> int:
-    cup = parse_cups(input[0])
-    last = cup.next
-    while last.next != cup:
-        last = last.next
+    cups = parse_cups(input[0])
+    start = int(input[0][0]) - 1
 
-    global N
-    for i in range(N, 1_000_000 + 1):
-        last.next = Cup(i, cup)
-        last = last.next
-        N = i
+    for i in range(len(cups), 1_000_000):
+        cups.append(i+1)
 
-    cups = play_rounds(cup, 10_000_000)
+    cups[len(cups) - 1] = start
+    cups[int(input[0][-1]) - 1] = len(input[0])
 
-    one_cup = find_cup(cup, 1)
-    return one_cup.next.label * one_cup.next.next.label
+    info("Cups: %s", print_cups(start, cups))
+    _, cups = play_rounds(start, cups, 10_000_000)
+
+    return (cups[0] + 1) * (cups[cups[0]] + 1)
