60dbba97c7fe6e2268ef873a92a849caa67b947e
Nicolas Deslandes <ndeslandes@live.fr>
2021-11-19 22:23:44 +0000

    Implementing Curve/Bits

--- 2016/Rust/day16/src/main.rs
+++ 2016/Rust/day16/src/main.rs
@@ -1,14 +1,39 @@
+#![feature(generators, generator_trait)]
+
+mod bits;
+mod curve;
+
+use bits::Bits;
+use curve::Curve;
+
 fn get_checksum_string(items: &Vec<char>, size: usize) -> String {
-    items.iter().take(size).fold(String::new(), |s, &ch| {
-        format!("{}{}", s, ch)
-    })
+    items
+        .iter()
+        .take(size)
+        .fold(String::new(), |s, &ch| format!("{}{}", s, ch))
+}
+
+// fn print_disk_old(items: &Vec<char>, size: usize) {
+//     println!("{} ({})", get_checksum_string(items, size), size);
+// }
+
+fn print_disk(items: &Vec<char>, size: usize) {
+    let d = Bits { bits: 13, len: 4 };
+    println!("Data: {}, rev: {}", d, d.rev());
+    println!("Data: {}, rev: {}", d.to_string(), d.rev().to_string());
+
+    let it = items.iter().take(size);
+    let values: Vec<char> = it.map(|&ch| ch).collect();
+
+    let output = "";
+    println!("{} ({})", output, size);
 }
 
 fn checksum(items: &mut Vec<char>, size: usize) -> String {
     let mut size = size;
     loop {
         // Size is guaranteed to be even here
-        println!("Checksum: {}", get_checksum_string(items, size));
+        print_disk(items, size);
 
         let mut index_in = 0;
         let mut index_out = 0;
@@ -27,31 +52,20 @@ fn checksum(items: &mut Vec<char>, size: usize) -> String {
             break;
         }
     }
-
+    // (10000101000010^2).1000010100001
+    print_disk(items, size);
     return get_checksum_string(items, size);
 }
 
 fn main() {
-    let target_size = 272;
+    let target_size = 800;
 
-    
+    let b = Bits::parse("10010");
+    println!("Bits: {}; reversed: {}", b, b.rev());
+    //let mut bits: Vec<char> = "01110110101001000".chars().collect();
+    let mut c = Curve::new("100001");
+    println!("Curve: {}", c);
 
-    let mut bits: Vec<char> = "01110110101001000".chars().collect();
-    let mut n = bits.len();
-    bits.extend(vec!['0';1000].iter());
- 
-    //println!("{} ({})", get_checksum_string(&bits, n), n);
-
-    while n < target_size {
-        bits[n] = '0';
-        for i in 1..(n + 1) {
-            bits[n + i] = if bits[n - i] == '0' { '1' } else { '0' };
-        }
-
-        n = n * 2 + 1;
-        println!("{} ({})", get_checksum_string(&bits, n), n)
-    }
-
-    let result = checksum(&mut bits, target_size);
-    println!("Result: {}", result);
+    c.expand(5500);
+    println!("After expansion: {}", c);
 }
