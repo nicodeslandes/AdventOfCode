500519c2ebcbc4eee9f70ce887a9e8e2c6f8b20c
Nicolas Deslandes <ndeslandes@live.fr>
2021-12-03 09:01:06 +0000

    Part 1 revisited

--- 2021/Rust/day03/src/main.rs
+++ 2021/Rust/day03/src/main.rs
@@ -2,6 +2,7 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::env;
 use std::fs::File;
+use std::io::Seek;
 
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
@@ -9,29 +10,29 @@ fn main() -> Result<()> {
     let file_name = env::args().nth(1).expect("Enter a file name");
 
     println!("Reading input from {}", file_name);
-    let file = File::open(file_name)?;
-    let lines = BufReader::new(file).lines().map(|line| line.unwrap());
-    let bits : Vec<_> =  lines.map(|line| line.chars().map(|ch| if ch == '0' {0} else {1}).collect::<Vec<_>>()).collect();
+    let mut file = File::open(file_name)?;
 
-    let mut bit_counts = vec![0;bits[0].len()];
+    let width = BufReader::new(&file).lines().nth(0).unwrap().unwrap().chars().count();
+    file.rewind()?;
+    let lines = BufReader::new(&file).lines().map(|line| line.unwrap());
 
-    for x in &bits {
-        for (i, b) in x.iter().enumerate(){
-            bit_counts[i] += b;
-        }
-    }
+    let numbers : Vec<_> =  lines
+        .map(|line| line.chars().fold(0, |acc, ch| (acc << 1) + if ch == '0' {0} else {1}))
+        .collect();
 
-    let mut gamma = 0;
-    let mut epsilon = 0;
-    for c in bit_counts.iter() {
-        let b = if *c < (bits.len() / 2) {0} else{1};
-        gamma <<=1;
-        gamma += b;
+    let count = numbers.len();
 
-        epsilon <<= 1;
-        epsilon += 1-b;
-    }
+    println!("Numbers: {:?}", numbers);
+    let gamma = (0..width).rev().fold(0, |acc, i|{
+        let mask = 1 << i;
+        println!("Mask: {}", mask);
+        let count_1: usize = numbers.iter().map(|n| (n&mask) >> i).sum();
+        println!("Count: {}", count_1);
+        (acc << 1) + if count_1 < count/2 {0} else {1}
+    });
     
+    let epsilon = (!gamma) & ((1<<width) - 1);
+
     println!("Gamma: {}, Epsilon: {}", gamma, epsilon);
     println!("Part 1: {}", gamma * epsilon);
     Ok(())
