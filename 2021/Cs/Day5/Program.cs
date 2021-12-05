89807405d16605b4231b703c73bcda6857be9309
Nicolas Deslandes <ndeslandes@live.fr>
2021-12-05 19:54:04 +0000

    Day 5 - Part 2 C# 😳

--- 2021/Cs/Day5/Program.cs
+++ 2021/Cs/Day5/Program.cs
@@ -1,7 +1,25 @@
 ﻿
 using System.Collections.Immutable;
 
-var count = Utils.ReadLinesFromInputFile(args)
+var count1 = ParseLines()
+    .Where(l => l.Start.X == l.End.X || l.Start.Y == l.End.Y)
+    .SelectMany(GeneratePositions)
+    .Aggregate(ImmutableDictionary.Create<Pos, int>(), (acc, pos) => acc.SetItem(pos, acc.GetValueOrDefault(pos) + 1))
+    .Values.Count(c => c > 1);
+
+var count2 = ParseLines()
+    .SelectMany(GeneratePositions)
+    .Aggregate(ImmutableDictionary.Create<Pos, int>(), (acc, pos) => acc.SetItem(pos, acc.GetValueOrDefault(pos) + 1))
+    .Values.Count(c => c > 1);
+
+Console.WriteLine("Part 1: {0}", count1);
+Console.WriteLine("Part 2: {0}", count2);
+
+
+
+IEnumerable<Line> ParseLines()
+{
+    return Utils.ReadLinesFromInputFile(args)
     .Select(line =>
     {
         var startEnd = line.Split(" -> ")
@@ -11,38 +29,27 @@ var count = Utils.ReadLinesFromInputFile(args)
             return new Pos(int.Parse(pos[0]), int.Parse(pos[1]));
         }).ToArray();
         return new Line(startEnd[0], startEnd[1]);
-    })
-    .Where(l => l.Start.X == l.End.X || l.Start.Y == l.End.Y)
-    .SelectMany(GeneratePositions)
-    .Aggregate(ImmutableDictionary.Create<Pos, int>(), (acc, pos) => acc.SetItem(pos, acc.GetValueOrDefault(pos) + 1))
-    .Values.Count(c => c > 1);
-
-Console.WriteLine("Part 1: {0}", count);
+    });
+}
 
 IEnumerable<Pos> GeneratePositions(Line line)
 {
-    if (line.Start.X == line.End.X)
+    var (start, end) = (line.Start, line.End);
+    var x_inc = end.X.CompareTo(start.X);
+    var y_inc = end.Y.CompareTo(start.Y);
+
+    var pos = start;
+    while(true)
     {
-        var inc = line.Start.Y < line.End.Y ? 1 : -1;
-        for (int y = line.Start.Y; y != line.End.Y + inc; y += inc)
-        {
-            yield return new Pos(line.Start.X, y);
-        }
-    }
-    else if (line.Start.Y == line.End.Y)
-    {
-        var inc = line.Start.X < line.End.X ? 1 : -1;
-        for (int x = line.Start.X; x != line.End.X + inc; x += inc)
-        {
-            yield return new Pos(x, line.Start.Y);
-        }
-    }
-    else
-    {
-        throw new InvalidOperationException("Nope!");
+        yield return pos;
+
+        if (pos == end) break;
+
+        pos.X += x_inc;
+        pos.Y += y_inc;
     }
 }
 
 
-readonly record struct Pos(int X, int Y);
+record struct Pos(int X, int Y);
 record Line(Pos Start, Pos End);
