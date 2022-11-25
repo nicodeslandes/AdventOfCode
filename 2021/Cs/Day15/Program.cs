7be5a401a587b8ba3aa782d1d8593d1d22dffa29
Nicolas Deslandes <ndeslandes@live.fr>
2022-11-25 07:11:10 +0000

    Day 15 - Part 2

--- 2021/Cs/Day15/Program.cs
+++ 2021/Cs/Day15/Program.cs
@@ -1,66 +1,151 @@
 ﻿using System.Diagnostics;
 
-var grid = ParseLines();
-var N = grid.Length;
-var min_lengths = grid.Select(row => new int[row.Length]).ToArray();
-var cellsToCheck = new PriorityQueue<Pos, int>();
-cellsToCheck.Enqueue(new(0, 0), 0);
-var cellHash = new HashSet<Pos> { new(0, 0) };
-
-int CellPriority(Pos pos) => pos.X + pos.Y; //* pos.X + pos.Y * pos.Y;
 var showGrid = false;
 
 if (showGrid) Console.Clear();
 var currentGridDisplay = new Dictionary<Pos, (int val, bool highlighted)>();
-for (int x = 0; x < N; x++)
+
+var grid = ParseLines();
+var sw = Stopwatch.StartNew();
+var part1 = Solve(grid);
+Console.WriteLine("Part 1: {0} ({1:N0} ms)", part1, sw.ElapsedMilliseconds);
+
+grid = ExpandGrid(grid, 5);
+var part2 = Solve(grid);
+Console.WriteLine("Part 2: {0} ({1:N0} ms)", part2, sw.ElapsedMilliseconds);
+
+int[][] ExpandGrid(int[][] grid, int factor)
 {
-    for (int y = 0; y < N; y++)
+    var N = grid.Length;
+    var newGrid = Enumerable.Range(0, N * factor).Select(_ => new int[N * factor]).ToArray();
+    for (int x = 0; x < factor; x++)
     {
-        currentGridDisplay[new(x, y)] = (-1, false);
+        for (int y = 0; y < factor; y++)
+        {
+            CopyInitialGrid(x * N, y * N, x + y);
+            DisplayGrid(newGrid);
+        }
     }
+
+    void CopyInitialGrid(int xDest, int yDest, int increase)
+    {
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                newGrid[yDest + y][xDest + x] = (grid[y][x] + increase - 1) % 9 + 1;
+            }
+        }
+    }
+
+    return newGrid;
+
 }
 
-var sw = Stopwatch.StartNew();
-while (cellsToCheck.Count > 0)
+int Solve(int[][] grid)
 {
-    var current = cellsToCheck.Dequeue();
-    if (showGrid) cellHash.Remove(current);
+    var N = grid.Length;
 
-    foreach (var cell in GetAdjacentCells(current))
+    var min_lengths = grid.Select(row => new int[row.Length]).ToArray();
+    var cellsToCheck = new PriorityQueue<Pos, int>();
+    cellsToCheck.Enqueue(new(0, 0), 0);
+    var cellHash = new HashSet<Pos> { new(0, 0) };
+
+    int CellPriority(Pos pos) => pos.X + pos.Y; //* pos.X + pos.Y * pos.Y;
+
+    DisplayGrid(grid);
+    var sw = Stopwatch.StartNew();
+    while (cellsToCheck.Count > 0)
     {
-        var pathLen = GetMinPathLen(cell);
-        if (min_lengths[cell.Y][cell.X] == 0 || pathLen < min_lengths[cell.Y][cell.X])
-        {
-            min_lengths[cell.Y][cell.X] = pathLen;
-            DisplayGrid();
+        var current = cellsToCheck.Dequeue();
+        if (showGrid) cellHash.Remove(current);
 
-            cellsToCheck.Enqueue(cell, CellPriority(cell));
-            if (showGrid) cellHash.Add(cell);
+        foreach (var cell in GetAdjacentCells(current))
+        {
+            var pathLen = GetMinPathLen(cell);
+            if (min_lengths[cell.Y][cell.X] == 0 || pathLen < min_lengths[cell.Y][cell.X])
+            {
+                min_lengths[cell.Y][cell.X] = pathLen;
+                DisplayGrid(min_lengths, cellHash);
+
+                cellsToCheck.Enqueue(cell, CellPriority(cell));
+                if (showGrid) cellHash.Add(cell);
+            }
+        }
+    }
+
+    return min_lengths[N - 1][N - 1];  
+
+    int GetMinPathLen(Pos pos)
+    {
+        var minAdjacentLength = GetAdjacentCells(pos)
+            .Where(HasMinLength)
+            .Select(c => min_lengths[c.Y][c.X])
+            .Concat(new[] { int.MaxValue })
+            .Min();
+
+        // If no adjacent cell has any path length yet, return the cell's value
+        if (minAdjacentLength == int.MaxValue) return grid[pos.Y][pos.X];
+
+        return minAdjacentLength + grid[pos.Y][pos.X];
+    }
+
+    bool HasMinLength(Pos pos) => min_lengths[pos.Y][pos.X] != 0;
+
+    IEnumerable<Pos> GetAdjacentCells(Pos pos)
+    {
+        for (int dx = -1; dx <= 1; dx++)
+        {
+            var x = pos.X + dx;
+            if (x < 0 || x >= grid[0].Length) continue;
+
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                if (dx == 0 && dy == 0) continue;
+
+                // Do not allow diagonal moves
+                if (dx * dy != 0) continue;
+
+                var y = pos.Y + dy;
+                if (y < 0 || y >= grid[0].Length) continue;
+                yield return new Pos(x, y);
+            }
         }
     }
 }
 
-Console.WriteLine("Part 1: {0} ({1:N0} ms)", min_lengths[N - 1][N-1], sw.ElapsedMilliseconds);
-
-void DisplayGrid()
+void DisplayGrid(int[][] g, ISet<Pos> highlights = null)
 {
     if (!showGrid) return;
 
+    var N = g.Length;
+
+    if (currentGridDisplay.Count < N * N)
+    {
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                currentGridDisplay[new(x, y)] = (-1, false);
+            }
+        }
+    }
+
     //Console.Write(".");
     //return;
-    //Thread.Sleep(100);
+    //Thread.Sleep(1000);
     for (int y = 0; y < N; y++)
     {
         for (int x = 0; x < N; x++)
         {
             var pos = new Pos(x, y);
-            var highlight = cellHash.Contains(pos);
-            var min = min_lengths[y][x];
+            var highlight = highlights?.Contains(pos) ?? false;
+            var min = g[y][x];
 
             var current = currentGridDisplay[pos];
             if (current != (min, highlight))
             {
-                Console.SetCursorPosition(2*x, y);
+                Console.SetCursorPosition(2 * x, y);
                 if (highlight) Console.BackgroundColor = ConsoleColor.Blue;
                 //Console.Write("{0}", grid[y][x] % 10);
                 //Console.SetCursorPosition(x, y + 1);
@@ -71,43 +156,6 @@ void DisplayGrid()
                 currentGridDisplay[pos] = (min, highlight);
                 Console.WriteLine();
             }
-        }
-    }
-}
-
-int GetMinPathLen(Pos pos)
-{
-    var minAdjacentLength = GetAdjacentCells(pos)
-        .Where(HasMinLength)
-        .Select(c => min_lengths[c.Y][c.X])
-        .Concat(new[] { int.MaxValue })
-        .Min();
-
-    // If no adjacent cell has any path length yet, return the cell's value
-    if (minAdjacentLength == int.MaxValue) return grid[pos.Y][pos.X];
-
-    return minAdjacentLength + grid[pos.Y][pos.X];
-}
-
-bool HasMinLength(Pos pos) => min_lengths[pos.Y][pos.X] != 0;
-
-IEnumerable<Pos> GetAdjacentCells(Pos pos)
-{
-    for (int dx = -1; dx <= 1; dx++)
-    {
-        var x = pos.X + dx;
-        if (x < 0 || x >= grid[0].Length) continue;
-
-        for (int dy = -1; dy <= 1; dy++)
-        {
-            if (dx == 0 && dy == 0) continue;
-
-            // Do not allow diagonal moves
-            if (dx * dy != 0) continue;
-
-            var y = pos.Y + dy;
-            if (y < 0 || y >= grid[0].Length) continue;
-            yield return new Pos(x, y);
         }
     }
 }
