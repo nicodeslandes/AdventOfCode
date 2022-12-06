335cd36bd9a991e47c947adf9f999e2db76b3a20
Nicolas Deslandes <ndeslandes@live.fr>
2022-12-06 07:09:40 +0000

    Day 6 - Part 1

---
+++ 2022/Cs/Day06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Part1: {0}", Part1());
+Console.WriteLine("Part2: {0}", Part2());
+
+
+int Part1()
+{
+    var signal = ReadInput();
+    return signal
+        .Select((ch, index) => (ch, index))
+        .OverlappingBuffer(4)
+        .Where(buffer => buffer.Select(t => t.ch).ToHashSet().Count == 4)
+        .Select(buffer => buffer[^1].index + 1)
+        .First();
+}
+
+string Part2()
+{
+    return null;
+}
+
+string ReadInput()
+{
+    return File.ReadAllText(args[0]);
+}
+
+public static class Ext
+{
+    public static IEnumerable<T[]> OverlappingBuffer<T>(this IEnumerable<T> src, int count)
+    {
+        var queue = new Queue<T>();
+        foreach (var item in src)
+        {
+            queue.Enqueue(item);
+            if (queue.Count == count)
+            {
+                yield return queue.ToArray();
+                queue.Dequeue();
+            }
+        }
+    }
+}
