0bdc8c726919fdea29d4b814546ec6bff5c0803a
Nicolas Deslandes <ndeslandes@live.fr>
2022-12-16 14:05:16 +0000

    Day 16 - Part 1 - Faster

--- 2022/Cs/Day16/Program.cs
+++ 2022/Cs/Day16/Program.cs
@@ -8,6 +8,7 @@ int Part1()
 {
     var nodes = ReadInput();
 
+    Console.WriteLine("Non-0 valves: {0}", string.Join(", ",nodes.Values.Where(v => v.FlowRate != 0).Select(v => v.Name)));
 
     return Solve(nodes);
 }
@@ -20,7 +21,7 @@ int Part2()
 int Solve(Dictionary<string, Node> nodes)
 {
     // For each possible state at time t, what is the released pressure at that point?
-    var states = new Dictionary<StateKey, State> { [new(ImmutableHashSet<string>.Empty, "AA")] = new(0, 0) };
+    var states = new Dictionary<StateKey, State> { [new(0, "AA")] = new(0, 0) };
     for (int time = 0; time < 30; time++)
     {
         Console.Write("{0}", time % 10);
@@ -32,9 +33,9 @@ int Solve(Dictionary<string, Node> nodes)
             var current = nodes[stateKey.CurrentNode];
 
             // Option 1: Open the tap
-            if (current.FlowRate != 0 && !stateKey.OpenNodes.Contains(current.Name))
+            if (current.FlowRate != 0 && !IsValveOpen(current, stateKey.OpenNodes))
             {
-                var s = stateKey with { OpenNodes = stateKey.OpenNodes.Add(current.Name) };
+                var s = stateKey with { OpenNodes = stateKey.OpenNodes | (1 << current.Index) };
                 var candidate = new State(state.TotalRate + current.FlowRate, state.ReleasedPressure + state.TotalRate);
 
                 // If the state is already there with a better released pressure, ignore the candidate
@@ -61,7 +62,10 @@ int Solve(Dictionary<string, Node> nodes)
         states = newStates;
     }
 
+    Console.WriteLine();
     return states.Values.Max(s => s.ReleasedPressure);
+
+    bool IsValveOpen(Node node, int openState) => ((1 << node.Index) & openState) != 0;
 }
 
 Dictionary<string, Node> ReadInput()
@@ -97,11 +101,17 @@ Dictionary<string, Node> ReadInput()
         }
     }
 
+    var index = 0;
+    foreach (var node in nodes.Values)
+    {
+        if (node.FlowRate > 0) node.Index = index++;
+    }
+
     return nodes;
 }
 
 record State(int TotalRate, int ReleasedPressure);
-record StateKey(ImmutableHashSet<string> OpenNodes, string CurrentNode);
+record StateKey(int OpenNodes, string CurrentNode);
 
 record Node(string Name, List<Node> Neighbours)
 {
@@ -112,6 +122,7 @@ record Node(string Name, List<Node> Neighbours)
     }
 
     public int FlowRate { get; set; }
+    public int Index { get; set; } = -1;
 }
 
 class Grid
