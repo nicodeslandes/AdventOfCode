2f89da7eb60c40ada62b478e1d902edc356f68f5
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-02 06:44:40 +0000

    Day 2 - Part 1

---
+++ 2023/Cs/Day02/Program.cs
@@ -0,0 +1,49 @@
+﻿using Humanizer;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+Console.WriteLine("Part1: {0}", Part1());
+Console.WriteLine("Part2: {0}", Part2());
+
+int Part1()
+{
+    var input = ReadInput();
+    return input
+        .Where(g => g.Draws.All(d =>
+            d.Cubes.Values.All(cs => cs switch
+            {
+                ("red", var count) => count <= 12,
+                ("green", var count) => count <= 13,
+                ("blue", var count) => count <= 14,
+                var x => throw new NotSupportedException($"Wrong colour: {x.Colour}"),
+            }
+        )))
+        .Sum(d => d.Id);
+}
+
+int Part2()
+{
+    return 0;
+}
+
+IEnumerable<Game> ReadInput()
+{
+    return File.ReadAllLines(args[0])
+        .Select(l => l.Split(": ") switch
+        {
+            [var g, var cs] => new Game(int.Parse(g.Split(" ")[1]), cs.Split("; ").Select(x => new Draw(
+                x.Split(", ").Select(x =>
+                x.Split(" ") switch
+                {
+                    [var c, var colour] => new CubeSet(colour, int.Parse(c)),
+                    var o => throw new NotSupportedException(o.ToString()),
+                }).ToDictionary(cs => cs.Colour))).ToArray()),
+            var o => throw new NotSupportedException(o.ToString()),
+        });
+}
+
+record Game(int Id, Draw[] Draws);
+record Draw(Dictionary<string, CubeSet> Cubes);
+record CubeSet(string Colour, int Count);
