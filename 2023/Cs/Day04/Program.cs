45112ed2b566b04b595d92479f89601b9ecf2244
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-04 08:05:36 +0000

    Day 04 - Part 1

--- 2023/Cs/Day04/Program.cs
+++ 2023/Cs/Day04/Program.cs
@@ -4,7 +4,8 @@ Console.WriteLine("Part2: {0}", Part2());
 int Part1()
 {
     var input = ReadInput();
-    return 0;
+
+    return input.Sum(card => card.cards.Where(card.winning.Contains).Aggregate(0, (score, _) => score == 0 ? 1 : score * 2));
 }
 
 int Part2()
@@ -12,12 +13,12 @@ int Part2()
     return 0;
 }
 
-Dictionary<(int x, int y), char> ReadInput()
+IEnumerable<(HashSet<int> winning, int[] cards)> ReadInput()
 {
-
     return File.ReadLines(args[0])
-        .Enumerate()
-        .SelectMany(l => l.value.Enumerate().Select(ch => (x: ch.index, y: l.index, ch: ch.value)))
-        .Where(x => x.ch != '.')
-        .ToDictionary(x => (x.x, x.y), x => x.ch);
+        .Select(l => l.Split(": ")[1].Split(" | ").Select(n => n.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)).ToArray() switch
+        {
+            [var w, var c] => (winning: w.ToHashSet(), cards: c.ToArray()),
+            var x => throw new NotSupportedException(x.ToString()),
+        });
 }
