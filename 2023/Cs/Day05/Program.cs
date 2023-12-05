cee794fd4570ddaee133a67fb61f3e49914977de
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-05 07:44:40 +0000

    Day 05 - Part 1

--- 2023/Cs/Day05/Program.cs
+++ 2023/Cs/Day05/Program.cs
@@ -1,36 +1,92 @@
 ﻿Console.WriteLine("Part1: {0}", Part1());
 Console.WriteLine("Part2: {0}", Part2());
 
-int Part1()
+long Part1()
+{
+    var (seeds, maps) = ReadInput();
+
+    return seeds.Min(i =>
+    {
+        var seed = i;
+        i = Map(i, maps.SeedToSoil);
+        i = Map(i, maps.SoilToFertilizer);
+        i = Map(i, maps.FertilizerToWater);
+        i = Map(i, maps.WaterToLight);
+        i = Map(i, maps.LightToTemperature);
+        i = Map(i, maps.TemperatureToHumidity);
+        i = Map(i, maps.HumidityToLocation);
+
+        //Console.WriteLine($"Seed {seed} -> {i}");
+        return i;
+    });
+
+    long Map(long value, RangeMap[] rangeMaps)
+    {
+        var mapped = rangeMaps.SelectNonNull(m => m.Map(value)).FirstOrDefault();
+        return mapped == 0 ? value : mapped;
+    }
+}
+
+long Part2()
 {
     var input = ReadInput();
-
-    return input.Sum(card => card.matchCount == 0 ? 0 : 1 << (card.matchCount - 1));
+    return 0;
 }
 
-int Part2()
+(long[] seeds, Maps maps) ReadInput()
 {
-    var input = ReadInput();
-    var counts = input
-        .Aggregate(ImmutableDictionary.Create<int, int>(),
-        (counts, c) =>
+    using var fileStream = File.OpenText(args[0]) ?? throw new Exception("File open");
+    var seeds = fileStream.ReadLine()?.Split(": ") switch
+    {
+        ["seeds", var seedValues] => seedValues.Split(" ").Select(long.Parse).ToArray(),
+        var x => throw new Exception($"Invalid line: {x}"),
+    };
+
+    fileStream.ReadLine();
+
+    var maps = new Maps(
+        ReadRangeMaps(),
+        ReadRangeMaps(),
+        ReadRangeMaps(),
+        ReadRangeMaps(),
+        ReadRangeMaps(),
+        ReadRangeMaps(),
+        ReadRangeMaps()
+        );
+
+    return (seeds, maps);
+
+    RangeMap[] ReadRangeMaps() =>
+        ReadRangeMapsRaw().OrderBy(m => m.sourceStart).ToArray();
+
+    IEnumerable<RangeMap> ReadRangeMapsRaw()
+    {
+        fileStream.ReadLine();
+        string line;
+        while (!string.IsNullOrEmpty(line = fileStream.ReadLine()))
         {
-            var currentCardCount = counts.GetValueOrDefault(c.nb) + 1;
-            counts = counts.SetItem(c.nb, currentCardCount);
-            counts = counts.SetItems(Enumerable.Range(c.nb + 1, c.matchCount).Select(newCard => KeyValuePair.Create(newCard, counts.GetValueOrDefault(newCard) + currentCardCount)));
-            return counts;
-        });
-    return counts.Values.Sum();
+            yield return line.Split(" ").Select(long.Parse).ToArray() switch
+            {
+                [var ss, var ds, var l] => new RangeMap(ds, ss, l),
+                var x => throw new Exception(x.ToString()),
+            };
+        }
+    }
+
 }
 
-IEnumerable<(int nb, HashSet<int> winning, int matchCount)> ReadInput()
+record RangeMap(long sourceStart, long destinationStart, long length)
 {
-    return File.ReadLines(args[0])
-        .Enumerate()
-        .Select(l => (l.index, value: l.value.Split(": ")[1].Split(" | ").Select(n => n.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)).ToArray() switch
-        {
-            [var w, var c] => (winning: w.ToHashSet(), cards: c),
-            var x => throw new NotSupportedException(x.ToString()),
-        }))
-        .Select(t => (nb: t.index + 1, t.value.winning, matchCount: t.value.cards.Count(t.value.winning.Contains)));
+    public long? Map(long src) => src < sourceStart ? src :
+        src >= sourceStart && src < sourceStart + length ? destinationStart + src - sourceStart :
+        null;
 }
+
+record Maps(
+    RangeMap[] SeedToSoil,
+    RangeMap[] SoilToFertilizer,
+    RangeMap[] FertilizerToWater,
+    RangeMap[] WaterToLight,
+    RangeMap[] LightToTemperature,
+    RangeMap[] TemperatureToHumidity,
+    RangeMap[] HumidityToLocation);
