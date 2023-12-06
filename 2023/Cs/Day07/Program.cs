78174e8f77580653c9dd7dafa59cdd0cbb1b3b4b
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-06 23:45:10 +0000

    Day 07 scaffolding

---
+++ 2023/Cs/Day07/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Part1: {0}", Part1());
+Console.WriteLine("Part2: {0}", Part2());
+
+long Part1()
+{
+    var input = ReadInput();
+
+    return 0;
+}
+
+long Part2()
+{
+    var input = ReadInput();
+
+    return 0;
+}
+
+IEnumerable<int> ReadInput()
+{
+    return [1, 2, 3];
+}
