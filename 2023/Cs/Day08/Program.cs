6d4498bd30549396b0abdb85b60dfefa71e84c4a
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-08 06:48:35 +0000

    Day 08 - Part 1

--- 2023/Cs/Day08/Program.cs
+++ 2023/Cs/Day08/Program.cs
@@ -3,9 +3,17 @@ Console.WriteLine("Part2: {0}", Part2());
 
 long Part1()
 {
-    var input = ReadInput();
+    var (moves, nodes)= ReadInput();
+    var current = "AAA";
+    int count = 0;
+    while (current != "ZZZ")
+    {
+        var choices = nodes[current];
+        current = moves[count % moves.Length] == 0 ? choices.left : choices.right;
+        count++;
+    }
 
-    return 0;
+    return count;
 }
 
 long Part2()
@@ -15,7 +23,35 @@ long Part2()
     return 0;
 }
 
-IEnumerable<int> ReadInput()
+(int[] moves, Dictionary<string, (string left, string right)> nodes) ReadInput()
 {
-    return [1, 2, 3];
+    int[]? moves = null;
+    Dictionary<string, (string left, string right)> nodes = new();
+
+    foreach (var line in Utils.ReadLinesFromInputFile(args))
+    {
+        if (moves == null)
+        {
+            moves = line.Select(ch => ch == 'L' ? 0 : 1).ToArray();
+            continue;
+        }
+
+        if (line == "") continue;
+
+        switch (line.Split(" = "))
+        {
+            case [var from, var next]:
+                nodes[from] = next[1..^1].Split(", ") switch
+                {
+                    [var left, var right] => (left, right),
+                    _ => throw new Exception("Error 2"),
+                };
+                ;
+                break;
+            default:
+                throw new Exception("Error 1");
+        }
+    }
+
+    return (moves!, nodes);
 }
