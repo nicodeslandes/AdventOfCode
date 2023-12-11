830c8602a80d19c427922ba1d6cb8d674e7fd8b6
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-11 19:35:09 +0000

    Day 11 - Part 1

--- 2023/Cs/Day11/Program.cs
+++ 2023/Cs/Day11/Program.cs
@@ -1,51 +1,78 @@
-﻿
-using System.Net.WebSockets;
-
-Console.WriteLine("Part1: {0}", Part1());
+﻿Console.WriteLine("Part1: {0}", Part1());
 Console.WriteLine("Part2: {0}", Part2());
 
 long Part1()
 {
-    var input = ReadInput();
-    return input.Sum(PredictNextValue);
-}
+    var input = ReadInput().ToArray();
+    var xSet = input.Select(p => p.X).Distinct().OrderBy(x => x).ToArray();
+    var ySet = input.Select(p => p.Y).Distinct().OrderBy(x => x).ToArray();
 
-long PredictNextValue(long[] values)
-{
-    var sum = 0L;
-    while (values.Length > 1 && values.Any(x => x != 0))
+    // x-coordinates expand for each value between positions, excepts for elements in xSet
+    // ---A--x1-----x2--x3--B
+    // The expansion here result in [AB] doubling, minus the 3 x between them
+    var xPrefixSums = ComputePrefixSums(xSet);
+    var yPrefixSums = ComputePrefixSums(ySet);
+
+    IEnumerable<(Position a, Position b)> GalaxyPairs()
     {
-        sum += values[^1];
-        values = values.Zip(values.Skip(1)).Select(x => x.Second - x.First).ToArray();
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                yield return (input[i], input[j]);
+            }
+        }
     }
 
-    return sum;
+    var totalXCount = xPrefixSums[^1];
+    var totalYCount = yPrefixSums[^1];
+    return GalaxyPairs().Select(p =>
+    {
+        var xCount = Math.Abs(xPrefixSums[p.a.X] - xPrefixSums[p.b.X]);
+        var yCount = Math.Abs(yPrefixSums[p.a.Y] - yPrefixSums[p.b.Y]);
+        return 2 * Distance(p.a, p.b) - xCount - yCount;
+    }).Sum();
 }
+
+int[] ComputePrefixSums(int[] xSet)
+{
+    var max = xSet[^1];
+    var result = new int[max + 1];
+    var xi = 0;
+    var sum = 0;
+    for (int i = 0; i <= max; i++)
+    {
+        if (i == xSet[xi])
+        {
+            sum++;
+            xi++;
+        }
+        result[i] = sum;
+    }
+
+    return result;
+}
+
+int Distance(Position a, Position b) => Math.Abs(b.X - a.X) + Math.Abs(b.Y - a.Y);
 
 long Part2()
 {
     var input = ReadInput();
-    return input.Sum(PredictPreviousValue);
+    return 0;
 }
 
-long PredictPreviousValue(long[] values)
+IEnumerable<Position> ReadInput()
 {
-    var alternateSum = 0L;
-    int i = 1;
-    while (values.Length > 1 && values.Any(x => x != 0))
-    {
-        alternateSum += Math.Sign(i) * values[0];
-        i = -i;
-        values = values.Zip(values.Skip(1)).Select(x => x.Second - x.First).ToArray();
-    }
-
-    return alternateSum;
-}
-
-IEnumerable<long[]> ReadInput()
-{
+    int y = 0;
     foreach (var line in Utils.ReadLinesFromInputFile(args))
     {
-        yield return line.Split(' ').Select(long.Parse).ToArray();
+        for (int x = 0; x < line.Length; x++)
+        {
+            if (line[x] == '#')
+            {
+                yield return new Position(x, y);
+            }
+        }
+        y++;
     }
 }
