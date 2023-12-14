66924aba4f5e60640db27e065cca9d59da8f8922
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-14 07:45:53 +0000

    Day 14 - Part 1

--- 2023/Cs/Day14/Program.cs
+++ 2023/Cs/Day14/Program.cs
@@ -1,59 +1,40 @@
-﻿Console.WriteLine("Part1: {0}", Part1());
+﻿
+
+Console.WriteLine("Part1: {0}", Part1());
 Console.WriteLine("Part2: {0}", Part2());
 
 long Part1()
 {
-    var input = ReadInput().ToArray();
-    var xSet = input.Select(p => p.X).Distinct().OrderBy(x => x).ToArray();
-    var ySet = input.Select(p => p.Y).Distinct().OrderBy(x => x).ToArray();
-
-    // x-coordinates expand for each value between positions, excepts for elements in xSet
-    // ---A--x1-----x2--x3--B
-    // The expansion here result in [AB] doubling, minus the 3 x between them
-    var xPrefixSums = ComputePrefixSums(xSet);
-    var yPrefixSums = ComputePrefixSums(ySet);
-
-    IEnumerable<(Position a, Position b)> GalaxyPairs()
-    {
-        for (int i = 0; i < input.Length; i++)
+    var (Y, rocks) = ReadInput();
+    var total = rocks
+        .GroupBy(r => r.Pos.X)
+        .OrderBy(g => g.Key)
+        .Select(column =>
         {
-            for (int j = i + 1; j < input.Length; j++)
+            var topSquareRock = 0;
+            var sum = 0L;
+            var nbPiledRocks = 0;
+            foreach (var r in column.OrderBy(x => x.Pos.Y))
             {
-                yield return (input[i], input[j]);
+                if (r.Type == RockType.Square)
+                {
+                    nbPiledRocks = 0;
+                    topSquareRock = r.Pos.Y + 1;
+                }
+                else
+                {
+                    sum += Y - nbPiledRocks - topSquareRock;
+                    nbPiledRocks++;
+                }
             }
-        }
-    }
 
-    var totalXCount = xPrefixSums[^1];
-    var totalYCount = yPrefixSums[^1];
-    return GalaxyPairs().Select(p =>
-    {
-        var xCount = Math.Abs(xPrefixSums[p.a.X] - xPrefixSums[p.b.X]);
-        var yCount = Math.Abs(yPrefixSums[p.a.Y] - yPrefixSums[p.b.Y]);
-        return 2 * Distance(p.a, p.b) - xCount - yCount;
-    }).Sum();
+            Console.WriteLine("Sum for column {0}: {1}", column.Key, sum);
+            return sum;
+        })
+        .Sum();
+
+    return total;
 }
-
-int[] ComputePrefixSums(int[] xSet)
-{
-    var max = xSet[^1];
-    var result = new int[max + 1];
-    var xi = 0;
-    var sum = 0;
-    for (int i = 0; i <= max; i++)
-    {
-        if (i == xSet[xi])
-        {
-            sum++;
-            xi++;
-        }
-        result[i] = sum;
-    }
-
-    return result;
-}
-
-int Distance(Position a, Position b) => Math.Abs(b.X - a.X) + Math.Abs(b.Y - a.Y);
 
 long Part2()
 {
@@ -61,18 +42,32 @@ long Part2()
     return 0;
 }
 
-IEnumerable<Position> ReadInput()
+(int y, Rock[] rocks) ReadInput()
 {
     int y = 0;
-    foreach (var line in Utils.ReadLinesFromInputFile(args))
+
+    IEnumerable<Rock> GetRocks()
     {
-        for (int x = 0; x < line.Length; x++)
+        foreach (var line in Utils.ReadLinesFromInputFile(args))
         {
-            if (line[x] == '#')
+            for (int x = 0; x < line.Length; x++)
             {
-                yield return new Position(x, y);
+                if (line[x] is '#' or 'O')
+                {
+                    yield return new Rock(new(x, y), line[x] == '#' ? RockType.Square : RockType.Round);
+                }
             }
+            y++;
         }
-        y++;
     }
+
+    var rocks = GetRocks().ToArray();
+    return (y, rocks);
+}
+
+record Rock(Position Pos, RockType Type);
+enum RockType
+{
+    Square,
+    Round
 }
