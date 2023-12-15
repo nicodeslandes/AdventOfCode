8def49996f3b273e0a8f95d7208441d01df124ad
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-15 07:30:54 +0000

    Day 15 - scaffolding

---
+++ 2023/Cs/Day15/Program.cs
@@ -0,0 +1,30 @@
+﻿using Move = (int dx, int dy);
+
+
+Console.WriteLine("Part1: {0}", Part1());
+Console.WriteLine("Part2: {0}", Part2());
+
+long Part1()
+{
+    var input = ReadInput();
+
+
+    return input
+        .Select(CalculateHash)
+        .Sum();
+}
+
+long Part2()
+{
+    return 0;
+}
+
+int CalculateHash(string instruction)
+{
+    return 0;
+}
+
+IEnumerable<string> ReadInput()
+{
+    return Utils.ReadLinesFromInputFile(args);
+}
