63310e77445f4f7181e157d8fcd0d0134ef60968
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-15 19:22:44 +0000

    Day 15 - Part 2

--- 2023/Cs/Day15/Program.cs
+++ 2023/Cs/Day15/Program.cs
@@ -1,4 +1,6 @@
-﻿using Move = (int dx, int dy);
+﻿using System.Collections;
+using System.Collections.Specialized;
+using Move = (int dx, int dy);
 
 
 Console.WriteLine("Part1: {0}", Part1());
@@ -16,7 +18,34 @@ long Part1()
 
 long Part2()
 {
-    return 0;
+    var input = ReadInput();
+    Dictionary<int, Box> boxes = new();
+
+    foreach (var instruction in input)
+    {
+        var actionIndex = instruction.IndexOfAny(['-', '=']);
+        var label = instruction[..actionIndex];
+
+        var boxId = CalculateHash(label);
+        if (!boxes.TryGetValue(boxId, out var box))
+        {
+            box = new(boxId);
+            boxes[boxId] = box;
+        }
+
+        if (instruction[actionIndex] == '=')
+        {
+            var focal = int.Parse(instruction[(actionIndex + 1)..]);
+
+            box.AddLens(label, focal);
+        }
+        else
+        {
+            box.RemoveLens(label);
+        }
+    }
+
+    return boxes.Values.Sum(b => b.FocusingPower);
 }
 
 int CalculateHash(string instruction)
@@ -35,4 +64,43 @@ int CalculateHash(string instruction)
 IEnumerable<string> ReadInput()
 {
     return Utils.ReadLinesFromInputFile(args).First().Split(",");
+}
+
+public class Box(int id)
+{
+    public int Id { get; } = id;
+
+    private OrderedDictionary _lenses = new();
+
+    public void AddLens(string label, int focal)
+    {
+        if (_lenses.Contains(label))
+        {
+            _lenses[label] = focal;
+        }
+        else
+        {
+            _lenses.Insert(_lenses.Count, label, focal);
+        }
+    }
+
+    public void RemoveLens(string label)
+    {
+        _lenses.Remove(label);
+    }
+
+    public int FocusingPower
+    {
+        get
+        {
+            var lensPowers = _lenses
+                .Cast<DictionaryEntry>()
+                .Enumerate()
+                .Select(x => (1 + Id) * (x.index + 1) * (int)(x.value.Value!))
+                .ToArray();
+
+            return lensPowers
+                .Sum();
+        }
+    }
 }
