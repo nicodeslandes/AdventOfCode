1cd1b0d55fd0ffc706064e60733f77b8851c2359
Nicolas Deslandes <ndeslandes@live.fr>
2023-12-21 07:19:15 +0000

    Day 21 - Scaffoling

---
+++ 2023/Cs/Day21/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Part1: {0}", Part1());
+Console.WriteLine("Part2: {0}", Part2());
+
+long Part1()
+{
+    var x = ReadInput();
+
+    return 0;
+}
+
+long Part2()
+{
+    var x = ReadInput();
+
+    return 0;
+}
+
+int ReadInput()
+{
+    var lines = Utils.ReadLinesFromInputFile(args);
+    return lines.Count();
+}
