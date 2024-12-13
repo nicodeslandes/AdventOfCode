9eb2d6f6068ab4ed5398f828922aac09fc52c41d
Nicolas Deslandes <ndeslandes@live.fr>
2024-12-13 08:11:18 +0000

    Day 03 - Part 1

---
+++ 2024/AdventOfCode2024/Day03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Part1: {0}", Part1());
+Console.WriteLine("Part2: {0}", Part2());
+
+int Part1()
+{
+    var input = ReadInput();
+
+    return input
+        .SelectMany(l => PuzzleRegex.Part1.Matches(l))
+        .Select(m =>
+        {
+            int x = int.Parse(m.Groups[1].ValueSpan);
+            int y = int.Parse(m.Groups[2].ValueSpan);
+            return x * y;
+        })
+        .Sum();
+    }
+
+int Part2()
+{
+    var input = ReadInput();
+    return 0;
+}
+
+IEnumerable<string> ReadInput()
+{
+    return Utils.ReadLinesFromInputFile(args);
+}
+
+static partial class PuzzleRegex
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    public static partial Regex Part1 { get; }
+}
